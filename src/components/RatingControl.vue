ae203807639826307ea741035f8bbd153020e908
fuadsuleyman <fuadsuleymanov@outlook.com>
2022-06-13 11:20:07 +0400

    custom control datan geted after form submit and selected button reseted also

--- src/components/RatingControl.vue
+++ src/components/RatingControl.vue
@@ -1,12 +1,12 @@
 <template>
   <ul>
-    <li :class="{ active: activeOption === 'poor' }">
+    <li :class="{ active: modelValue === 'poor' }">
       <button type="button" @click="activate('poor')">Poor</button>
     </li>
-    <li :class="{ active: activeOption === 'average' }">
+    <li :class="{ active: modelValue === 'average' }">
       <button type="button" @click="activate('average')">Average</button>
     </li>
-    <li :class="{ active: activeOption === 'great' }">
+    <li :class="{ active: modelValue === 'great' }">
       <button type="button" @click="activate('great')">Great</button>
     </li>
   </ul>
@@ -16,16 +16,9 @@
 export default {
   props: ['modelValue'],
   emits: ['update:modelValue'],
-  data() {
-    return {
-      activeOption: this.modelValue,
-    };
-  },
   methods: {
     activate(option) {
-      this.activeOption = option;
-      this.$emit('update:modelValue', option)
-      console.log(`active button is pressed!`);
+      this.$emit('update:modelValue', option);
     },
   },
 };
